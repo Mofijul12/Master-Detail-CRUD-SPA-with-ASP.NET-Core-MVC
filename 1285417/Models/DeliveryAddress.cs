8a50a7b41478bb63434207ae11fafeacec0f826c
Mofijul12 <mi6381176@gmail.com>
2025-06-18 07:14:01 -0600

    Initial Commit

---
+++ 1285417/Models/DeliveryAddress.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace _1285417.Models
+{
+    public class DeliveryAddress
+    {
+        public int DeliveryAddressId { get; set; }
+        [Required, ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        [Required, StringLength(60)]
+        public string ContactPerson { get; set; } = default!;
+        [Required, StringLength(30)]
+        public string Phone { get; set; } = default!;
+        [Required, StringLength(100)]
+        public string Address { get; set; } = default!;
+        public virtual Customer? Customer { get; set; }
+    }
+}
---
+++ 1285417/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1285417.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
